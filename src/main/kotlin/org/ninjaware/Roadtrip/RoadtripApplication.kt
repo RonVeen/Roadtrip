868c2dbf7d02bf2874368b9eaf9d237b4f743b08
RonVeen <ron.veen@kadaster.nl>
2018-01-29 15:21:12 +0100

    Initial commit

---
+++ src/main/kotlin/org/ninjaware/Roadtrip/RoadtripApplication.kt
@@ -0,0 +1,11 @@
+package org.ninjaware.Roadtrip
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoadtripApplication
+
+fun main(args: Array<String>) {
+    runApplication<RoadtripApplication>(*args)
+}
